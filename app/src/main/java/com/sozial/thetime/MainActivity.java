17aa417f31a8ac17a4253fbbb56805f5963b9eac
ibrahim <ibr.12399@gmail.com>
2016-09-19 21:09:34 +0400

    just a commit

---
+++ app/src/main/java/com/sozial/thetime/MainActivity.java
@@ -0,0 +1,83 @@
+package com.sozial.thetime;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public ArrayList<String> CountDR (long Duration){
+
+        Duration = 7200000;
+        long Hours = Duration / (60 * 60 * 1000);
+        Duration =- Hours%(60 * 60 * 1000);
+        long Minuites = Duration / (60 * 1000);
+        Duration =- Minuites%(60 * 1000);
+        long Seconds = Duration / (1000);
+        String h = String.valueOf(Hours);
+        String m = String.valueOf(Minuites);
+        String s = String.valueOf(Seconds);
+
+        ArrayList<String> a = new ArrayList<String>();
+        a.add(0, h);
+        a.add(1, m);
+        a.add(2, s);
+        return a;
+
+
+
+    }
+}
